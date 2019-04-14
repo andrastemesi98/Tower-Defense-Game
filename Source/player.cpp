893afec9c2e03267c2fc159609a7b9ce1129c384
Morassi Dávid <c2yf8n@inf.elte.hu>
2019-04-14 23:19:11 +0200

    Replace player.cpp

--- Source/player.cpp
+++ Source/player.cpp
@@ -3,30 +3,40 @@
 namespace model{
 
 player::player(const std::string& nin, int def_gold){
-	_name=nin;
-	_gold=def_gold;
+    _name=nin;
+    _gold=def_gold;
 }
 
 void player::addGold(int amount){
-	_gold+=amount;
+    _gold+=amount;
 }
 
 void player::removeUnit(unit* myUnit){
-	_units.erase(std::remove(_units.begin(), _units.end(), myUnit), _units.end());
+    _units.erase(std::remove(_units.begin(), _units.end(), myUnit), _units.end());
 }
 
 void player::removeTower(tower* myTower){
-        _towers.erase(std::remove(_towers.begin(), _towers.end(), myTower), _towers.end());
+        //_towers.erase(std::remove(_towers.begin(), _towers.end(), myTower), _towers.end());
+        //field* f;
+        //f=myTower->location();
+        //f->deleteTower();
+        //~myTower();
 }
 
 bool player:: placeTower(int ID, field* loc){
-	//TODO
-	return true;
+    //tower* t;
+    //t= new tower(this, loc, iddmg, idrng);
+    //_towers.push_back(t);
+    //return loc->addTower(t);
+    return true;
 }
 
 bool player:: placeCreature(int ID, field* loc){
-	//TODO
-	return true;
+    //unit* u;
+    //u= new unit(this, basefield, iddmg, idrng, idsp);
+    //_units.push_back(u);
+    //return basefield->addUnit(u);
+    return true;
 }
 
 void player::update(){
