98a0c8186ece60a4d44bc3a074576ad2ce152aef
Morassi Dávid <c2yf8n@inf.elte.hu>
2019-04-14 23:21:17 +0200

    Eventek kezelése, tényleges működéshez hiányzó részek leírása

--- Source/view.cpp
+++ Source/view.cpp
@@ -1,6 +1,7 @@
 #include "../Include/view.h"
 #include <QMessageBox>
 
+
 view::view::view(QWidget *parent) : QWidget(parent)
 {
     resize(900, 900);
@@ -47,19 +48,27 @@ void view::view::run()
         }
 
         for (int i = 0; i < buttonTable.size(); i++)
+        {
+            for(int j = 0; j < buttonTable[0].size(); j++)
             {
-                for(int j = 0; j < buttonTable[0].size(); j++)
-                {
-                    buttonTable[i][j] = new QPushButton();
-                    buttonTable[i][j]->setFont(QFont("Times New Roman", 10, QFont::Bold));
-                    buttonTable[i][j]->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
-                    tableLayout->addWidget(buttonTable[i][j], i, j);
-                    buttonTable[i][j]->setStyleSheet("background-color:orange;");
+                buttonTable[i][j] = new QPushButton();
+                buttonTable[i][j]->setFont(QFont("Times New Roman", 10, QFont::Bold));
+                buttonTable[i][j]->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+                tableLayout->addWidget(buttonTable[i][j], i, j);
+                buttonTable[i][j]->setStyleSheet("background-color:orange;");
 
-                    connect(buttonTable[i][j], SIGNAL(clicked()), this, SLOT(buttonClicked()));
-                }
+                connect(buttonTable[i][j], SIGNAL(clicked()), this, SLOT(buttonClicked()));
             }
-        //newGame(_sizex, _sizey);
+        }
+        newGame(_sizex, _sizey);
+
+        if(!timerExists)
+        {
+            _timer = new QTimer(this);
+            connect(_timer, SIGNAL(timeout()), this, SLOT(update()));
+            _timer->start(100);
+            timerExists = true;
+        }
     }
 }
 
@@ -69,6 +78,13 @@ void view::view::settings()
     {
         inSettings = true;
         inNewgame = false;
+        if(timerExists)
+        {
+            _timer->stop();
+            timerExists = false;
+            positionx = 0;
+            positiony = 0;
+        }
 
         for (int i = 0; i < buttonTable.size(); i++)
         {
@@ -80,12 +96,12 @@ void view::view::settings()
         }
 
         //ha nem léteznek, akkor kell létrehozni őket:
-        _infoLabel = new QLabel("Az 1. játékos (piros) az egér bal gombjával tornyot helyezhet le adott mezőre, a bázisára kattintva egységet indíthat onnan.\n\
-    A 2. játékos (zöld) a nyilakkal lépkedve kiválaszthatja a mezőt, melyre tornyot akar építeni, e körül szaggatott vonal jelenik meg.\n\
-    A ’t’ gomb megnyomására megépül a torony. Az ’e’ gomb megnyomására 1 egység indul a bázisából.\n\
-    Rövidítve emlékeztető:\n\
-    Első (piros) játékos: torony – bal egérgomb mezőre, egység – bal egérgomb bázisra.\n\
-    Második (zöld) játékos: mezőválasztás – nyilak, torony – ’t’, egység – ’e’.");
+        _infoLabel = new QLabel("Az 1. játékos (piros) az egér bal gombjával tornyot helyezhet le adott mezőre, a '0'-t nyomva egységet indíthat.\n\
+A 2. játékos (zöld) a 'WASD' gombokkal lépkedve kiválaszthatja a mezőt, melyre tornyot akar építeni, ezen 'T' jelenik meg.\n\
+A ’t’ gomb megnyomására megépül a torony. Az ’e’ gomb megnyomására 1 egység indul a bázisából.\n\
+Rövidítve emlékeztető:\n\
+Első (piros) játékos: torony – bal egérgomb mezőre, egység – '0'.\n\
+Második (zöld) játékos: mezőválasztás – 'WASD', torony – ’t’, egység – ’e’.");
         infos->addWidget(_infoLabel, 0, Qt::AlignHCenter);
 
         sizeButtons = new QHBoxLayout();
@@ -109,6 +125,25 @@ void view::view::exit()
 {
     QMessageBox::information(this, "Kilépés", "A játékablak bezárul!");
     QApplication::quit();
+}
+
+void view::view::update()
+{
+    for (int i = 0; i < buttonTable.size(); i++)
+    {
+        for(int j = 0; j < buttonTable[0].size(); j++)
+        {
+            buttonTable[i][j]->setText("");
+//ha 1. játékos bázisa, akkor buttonTable[i][j]->setStyleSheet("background-color:darkRed;");
+//ha 1. játékos tornya, akkor buttonTable[i][j]->setStyleSheet("background-color:red;");
+//ha 2. játékos bázisa, akkor buttonTable[i][j]->setStyleSheet("background-color:darkGreen;");
+//ha 2. játékos tornya, akkor buttonTable[i][j]->setStyleSheet("background-color:lightGreen;");
+//ha 1. játékos egysége, akkor buttonTable[i][j]->setStyleSheet("background-color:white;");
+//ha 2. játékos egysége, akkor buttonTable[i][j]->setStyleSheet("background-color:black;");
+//ha mindkettő egysége, akkor rá kép
+        }
+    }
+    buttonTable[positionx][positiony]->setText("T");
 }
 
 void view::view::setSmallGame()
@@ -135,15 +170,40 @@ void view::view::buttonClicked()
     uint a, b;
     a = nr / _sizex;
     b = nr % _sizex;
+    //game-nek az a fv-e, ami 1. játékosnak (a,b)-re tornyot rak
 }
 
 void view::view::keyPressEvent(QKeyEvent *event)
 {
     if(inNewgame)
     {
+        if(event->key() == Qt::Key_0)
+        {
+            //game-nek az a fv-e, amelyik indít 1. játékosnak unitot
+        }
+        if(event->key() == Qt::Key_W)
+        {
+            if(positionx > 0) positionx--;
+        }
+        if(event->key() == Qt::Key_S)
+        {
+            if(positionx < _sizex - 1) positionx++;
+        }
+        if(event->key() == Qt::Key_A)
+        {
+            if(positiony > 0) positiony--;
+        }
+        if(event->key() == Qt::Key_D)
+        {
+            if(positiony < _sizey - 1) positiony++;
+        }
         if(event->key() == Qt::Key_T)
         {
-            //...
+            //game-nek az a fv-e, ami 2. játékosnak (posx, posy)-ra tornyot rak
+        }
+        if(event->key() == Qt::Key_E)
+        {
+            //game-nek az a fv-e, amelyik indít 2. játékosnak unitot
         }
     }
 }
