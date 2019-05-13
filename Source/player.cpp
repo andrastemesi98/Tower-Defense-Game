4df4e20a9c2e1b449b9941ac82ae8fba7bf574c3
Morassi Dávid <c2yf8n@inf.elte.hu>
2019-05-13 21:10:18 +0200

    Apró javítás

--- Source/player.cpp
+++ Source/player.cpp
@@ -57,9 +57,9 @@ std::vector<field*> buf;
 bool player:: placeTower(int ID, field* loc){
     if(_gold>=100){
         tower *t = new tower(this, loc, 1, 1);
-        _towers.push_back(t);
         bool b = loc->addTower(t);
         if( ! b ) return false;
+        _towers.push_back(t);
         // remove tower if neccessary:
         if ( ! dijkstra(buf, _base->location(), getEnemyPlayer()->_base->location()) ) {
             _towers.resize( _towers.size() -1 );
@@ -89,10 +89,10 @@ bool player:: placeCreature(int ID, field* loc){
 }
 
 void player::update(){
-    for ( uint i = 0; i < _units.size(); ++i) {
+    /*for ( uint i = 0; i < _units.size(); ++i) {
         std::cerr << "position of unit " << i << " of player " << this->ID() << ": " <<
                      _units[i]->location()->x << ";" << _units[i]->location()->y << std::endl;
-    }
+    }*/
     for( auto u : _removed_units) {
         _units.erase(std::remove(_units.begin(), _units.end(), u), _units.end());
     }
